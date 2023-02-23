fe69546018f5f361d77ee72a16e3ecebd37981df
Thomas <tbui0525@gmail.com>
2023-02-23 10:52:31 -0800

    SQLFluff sucks

--- hw_02.sql
+++ hw_02.sql
@@ -1,78 +1,105 @@
 USE baseball;
-
-
-
 #HISTORIC_BA
-#DROP TEMPORARY TABLE historic_ba;
-CREATE TABLE historic_ba (batter varchar(6), Hits int, atBats int, BA float)
-SELECT bc.batter, sum(bc.Hit) as Hits,
-       sum(bc.atBat) as atBats, sum(bc.Hit)/sum(bc.atBat) as BA
+#I do not know why I kept getting a parse error
+#But it went away when I used INSERT INTO, so I'mma stick with that.
+
+CREATE TABLE historic_ba(
+    batter INT,
+    BA FLOAT
+)
+;
+
+INSERT INTO historic_ba
+SELECT
+    batter AS batter,
+    SUM(Hit) / NULLIF(SUM(atBat), 0) AS BA
+#NULLIF statement comes from w3schools.com/sql/sql_null_values.asp
 FROM batter_counts bc
-JOIN game
-    on bc.game_id = game.game_id
-    GROUP BY bc.batter
-    HAVING sum(bc.atBat) != 0;
+    JOIN game
+        ON bc.game_id = game.game_id
+GROUP BY bc.batter
+;
+#I got this code to turn NULLs into 0's from this website
+#
+UPDATE historic_ba SET BA = 0 WHERE BA IS NULL;
 
-
-
-SELECT *
-FROM historic_ba
-    LIMIT 0,20;
 #ANNUAL_BA
-#DROP TEMPORARY TABLE annual_ba
-CREATE TABLE annual_ba (batter varchar(6), batting_year year, Hits int, atBats int, BA float)
-SELECT bc.batter,year(game.local_date) as batting_year, sum(bc.Hit) as Hits,
-       sum(bc.atBat) as atBats, sum(bc.Hit)/sum(bc.atBat) as BA
+CREATE TABLE annual_ba(
+    batter INT,
+    batting_year YEAR,
+    Hits INT,
+    atBats INT,
+    BA FLOAT
+)
+;
+INSERT INTO annual_ba
+SELECT
+    batter AS batter,
+    YEAR(game.local_date) AS batting_year,
+    SUM(bc.Hit) AS Hits,
+    SUM(bc.atBat) AS atBats,
+    SUM(bc.Hit) / NULLIF(SUM(bc.atBat), 0) AS BA
 FROM batter_counts bc
-JOIN game
-    on bc.game_id = game.game_id
-    GROUP BY bc.batter, year(game.local_date)
-    HAVING sum(bc.atBat) != 0;
+    JOIN game
+        ON bc.game_id = game.game_id
+GROUP BY bc.batter, YEAR(game.local_date)
+;
 
-SELECT *
-FROM annual_ba
-LIMIT 0,20;
 
-SELECT *
-FROM historic_ba
-LIMIT 0,20;
-
+UPDATE annual_ba SET BA = 0 WHERE BA IS NULL;
 #ROLLING AVERAGE
-DROP TEMPORARY TABLE each_day_ba;
-CREATE TEMPORARY TABLE each_day_ba (batter int, game_id int,
-local_date date, Hits int, atBats int, index(batter, game_id))
-SELECT bc.batter, game.game_id as game_id, date(game.local_date) as local_date, sum(bc.Hit) as Hits,
-       bc.atBat as atBats
-FROM batter_counts bc
-JOIN game
-    on bc.game_id = game.game_id
-    GROUP BY bc.batter, game.local_date;
+DROP TEMPORARY TABLE IF EXISTS each_day_ba;
+CREATE TEMPORARY TABLE each_day_ba(
+    batter INT,
+    game_id INT,
+    local_date DATE,
+    Hits INT,
+    atBats INT,
+    INDEX(batter, game_id)
+)
+;
+INSERT INTO each_day_ba
+SELECT
+    batter AS batter,
+    game.game_id AS game_id,
+    DATE(game.local_date) AS local_date,
+    SUM(Hit) AS Hits,
+    atBat AS atBats
+FROM batter_counts
+    JOIN game
+        ON batter_counts.game_id = game.game_id
+GROUP BY batter_counts.batter, game.local_date
+;
 
-
-
-CREATE TABLE ROLLING_BA (batter int, game_id int,local_date date, first_date date,
-last_date date,  Total_Hits int, Total_atBats int, BA float)
-SELECT bc.batter,
-       bc.game_id,
-
-       date(game.local_date) 'local_date',
-    MIN(each_day_ba.local_date) 'first_date',
-    MAX(each_day_ba.local_date) 'last_date',
-    SUM(each_day_ba.Hits) 'Total_Hits',
-    SUM(each_day_ba.atBats) 'Total_atBats',
-    SUM(each_day_ba.Hits)/SUM(each_day_ba.atBats) as 'BA'
+DROP TABLE IF EXISTS ROLLING_BA;
+CREATE TABLE ROLLING_BA(
+    batter INT,
+    game_id INT,
+    local_date DATE,
+    first_date DATE,
+    last_date DATE,
+    Total_Hits INT,
+    Total_atBats INT,
+    BA FLOAT
+)
+;
+INSERT INTO ROLLING_BA
+SELECT
+    each_day_ba.batter AS batter,
+    game.game_id AS game_id,
+    DATE(game.local_date) AS local_date,
+    MIN(each_day_ba.local_date) AS first_date,
+    MAX(each_day_ba.local_date) AS last_date,
+    SUM(each_day_ba.Hits) AS Total_Hits,
+    SUM(each_day_ba.atBats) AS Total_atBats,
+    SUM(each_day_ba.Hits) / NULLIF(SUM(each_day_ba.atBats), 0) AS BA
 FROM each_day_ba
-JOIN batter_counts bc on bc.batter = each_day_ba.batter
-JOIN game on game.game_id = bc.game_id
-WHERE date(game.local_date) > date(each_day_ba.local_date)
-and  date_sub(date(game.local_date), INTERVAL 101 DAY) <= each_day_ba.local_date
+    JOIN batter_counts ON batter_counts.batter = each_day_ba.batter
+    JOIN game ON game.game_id = batter_counts.game_id
+WHERE DATE(game.local_date) > DATE(each_day_ba.local_date)
+    AND  DATE_SUB(DATE(game.local_date), INTERVAL 101 DAY) <= each_day_ba.local_date
 #https://mariadb.com/kb/en/date_sub/ where I got date_sub
 #101 because 100 days starting from previous day
-GROUP BY bc.batter, bc.game_id
-HAVING SUM(each_day_ba.atBats) !=0;
-
-
-
-SELECT * FROM ROLLING_BA
-LIMIT 0,20;
-
+GROUP BY batter_counts.batter, batter_counts.game_id
+;
+UPDATE ROLLING_BA SET BA = 0 WHERE BA IS NULL;
